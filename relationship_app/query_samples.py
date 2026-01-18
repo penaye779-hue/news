b3d9754b2b652252394d18b2b12277c1492bf240
Peniel Negasa <penaye779@gmail.com>
2026-01-18 14:44:45 +0300

    Fresh ALX Django project with relationship_app, models, and query_samples

---
+++ relationship_app/query_samples.py
@@ -0,0 +1,12 @@
+from .models import Author, Book, Library, Librarian
+
+def books_by_author(author_name):
+    return Book.objects.filter(author__name=author_name)
+
+def books_in_library(library_name):
+    library = Library.objects.get(name=library_name)
+    return library.books.all()
+
+def librarian_for_library(library_name):
+    library = Library.objects.get(name=library_name)
+    return Librarian.objects.get(library=library)
