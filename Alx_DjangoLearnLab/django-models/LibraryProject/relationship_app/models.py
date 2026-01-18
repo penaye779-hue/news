622199c42b4d7f6cb07c508fc11a4a06d5f3acdf
Peniel Negasa <penaye779@gmail.com>
2026-01-18 14:26:51 +0300

    Move relationship_app inside LibraryProject and register app

--- Alx_DjangoLearnLab/django-models/LibraryProject/relationship_app/models.py
+++ Alx_DjangoLearnLab/django-models/LibraryProject/relationship_app/models.py
@@ -1,19 +1,15 @@
 from django.db import models
 
-
 class Author(models.Model):
     name = models.CharField(max_length=255)
-
 
 class Book(models.Model):
     title = models.CharField(max_length=255)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
 
-
 class Library(models.Model):
     name = models.CharField(max_length=255)
     books = models.ManyToManyField(Book)
-
 
 class Librarian(models.Model):
     name = models.CharField(max_length=255)
--- Alx_DjangoLearnLab/django-models/LibraryProject/relationship_app/query_samples.py
+++ Alx_DjangoLearnLab/django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,15 +1,12 @@
-from relationship_app.models import Author, Book, Library, Librarian
+from .models import Author, Book, Library, Librarian
 
+def books_by_author(author_name):
+    return Book.objects.filter(author__name=author_name)
 
-# Query all books by a specific author
-author = Author.objects.get(name="Author Name")
-books_by_author = Book.objects.filter(author=author)
+def books_in_library(library_name):
+    library = Library.objects.get(name=library_name)
+    return library.books.all()
 
-
-# List all books in a library
-library = Library.objects.get(name="Library Name")
-books_in_library = library.books.all()
-
-
-# Retrieve the librarian for a library
-librarian = Librarian.objects.get(library=library)
+def librarian_for_library(library_name):
+    library = Library.objects.get(name=library_name)
+    return Librarian.objects.get(library=library)
