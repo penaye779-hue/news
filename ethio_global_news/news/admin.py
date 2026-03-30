d9f7987d043579e87baff0365740d99d23fce6ef
Peniel Negasa <penaye779@gmail.com>
2026-03-30 15:43:50 +0300

    Fix project structure for Render deployment

--- ethio_global_news/news/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Category, Newsletter, Subscriber
-
-# Register models so they show in admin
-admin.site.register(Category)
-admin.site.register(Newsletter)
-admin.site.register(Subscriber)
